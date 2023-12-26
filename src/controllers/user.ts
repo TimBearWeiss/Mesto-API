e98a6d6730d1f6453c6ab6e88a9a60e620348902
Dmitrii Zverev <104170542+TimBearWeiss@users.noreply.github.com>
2023-12-26 23:10:18 +0300

    Add files via upload

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import User from "../models/user";
 
 const getUsers = (req: Request, res: Response) => {};
 
 const getCurrentUser = (req: Request, res: Response) => {};
 
-const CreateUser = (req: Request, res: Response) => {};
+const CreateUser = (req: Request, res: Response) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    // вернём записанные в базу данные
+    .then((user) => res.send({ data: user }))
+    // данные не записались, вернём ошибку
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+};
