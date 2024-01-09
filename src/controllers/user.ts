4218e1814df9ef6abce552fc1ad8a298ea37fa6d
Dmitrii Zverev <104170542+TimBearWeiss@users.noreply.github.com>
2024-01-09 23:17:02 +0300

    controllers

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
-const getUsers = (req: Request, res: Response) => {};
+export const getUsers = (req: Request, res: Response) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
 
-const getCurrentUser = (req: Request, res: Response) => {};
+export const getCurrentUser = (req: Request, res: Response) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send("Пользователь не найден");
+      }
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send("Произошла ошибка при поиске пользователя: " + error);
+    });
+};
 
-const CreateUser = (req: Request, res: Response) => {
+export const CreateUser = (req: Request, res: Response) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
